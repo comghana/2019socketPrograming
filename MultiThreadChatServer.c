d072eb23d571c877dd1dd702c7f9e39c76761e2d
comghana <46733911+comghana@users.noreply.github.com>
2019-12-03 00:47:45 +0900

    Update MultiThreadChatServer.c

--- MultiThreadChatServer.c
+++ MultiThreadChatServer.c
@@ -6,136 +6,188 @@
 #include <sys/socket.h>
 #include <pthread.h>
 
-void *do_chat(void *);
-int pushClient(int, char *);
+#define IPADDR "127.0.0.1"
+#define PORT 9000
+#define CHATDATA 1024
+#define MAX_CLIENT 10
+#define INVALID_SOCK -1
+
+typedef struct s_user{
+	int c_socket;
+	char nickname[20];
+	char room[20];
+}user;
+
+void *doChat(void *);
+int pushClient(int);
 int popClient(int);
-pthread_t thread;
+
+pthread_t th;
 pthread_mutex_t mutex;
 
-#define MAX_CLIENT 10
-#define CHATDATA 1024
-#define INVALID_SOCK -1
-#define PORT 9000
+user listClient[MAX_CLIENT];
+char greeting[] = "[NOTICE] Welcome to chatting room\n";
+char CODE200[] = "[NOTICE] Sorry No More Connection\n";
 
-#define WHISPER "/w"
-#define DELIMETER " "
-struct chatClient{
-	int c_socket;
-	char nickname[CHATDATA];
-};
-struct chatClient clientList[MAX_CLIENT];
-char	escape[ ] = "exit";
-char	greeting[ ] = "Welcome to chatting room\n";
-char	CODE200[ ] = "Sorry No More Connection\n";
-
-int main(int argc, char *argv[ ])
-{
-
+int main(int argc, int *argv[]){
 	int c_socket, s_socket;
 	struct sockaddr_in s_addr, c_addr;
-	int	len;
-	int	i, j, n;
-	int	res;
-	char nickname[CHATDATA];
-	if(pthread_mutex_init(&mutex, NULL) != 0) {
-		printf("Can not create mutex\n");
-		return -1;
-	}
+	int len;
+	int i;
+	int th_id;
+	int res;
+
 	s_socket = socket(PF_INET, SOCK_STREAM, 0);
+
 	memset(&s_addr, 0, sizeof(s_addr));
-	s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	s_addr.sin_addr.s_addr = inet_addr(IPADDR);
 	s_addr.sin_family = AF_INET;
 	s_addr.sin_port = htons(PORT);
-	if(bind(s_socket, (struct sockaddr *)&s_addr, sizeof(s_addr)) == -1) {
-		printf("Can not Bind\n");
-		return -1;
+
+	if(bind(s_socket, (struct sockaddr *) &s_addr, sizeof(s_addr))==-1){
+		printf("[ERROR] Can not bind.\n");
+	}
+	if(listen(s_socket, 5)==-1){
+		printf("[ERROR] Listen fail.\n");
 	}
 
-	if(listen(s_socket, MAX_CLIENT) == -1) {
-		printf("listen Fail\n");
-		return -1;
+	for(i=0; i<MAX_CLIENT; i++){
+		listClient[i].c_socket = INVALID_SOCK;
 	}
-	for(i = 0; i < MAX_CLIENT; i++)
-		clientList[i].c_socket = INVALID_SOCK;
-	while(1) {
+	while(1){
 		len = sizeof(c_addr);
 		c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
-		if((n = read(c_socket, nickname, sizeof(nickname))) < 0){
-			printf("nickname read fail.\n");
-			return -1;
-		}	
-		res = pushClient(c_socket, nickname);
-		if(res < 0) {
+		res = pushClient(c_socket);
+		if(res < 0){
 			write(c_socket, CODE200, strlen(CODE200));
 			close(c_socket);
-		} else {
+		}
+		else{
 			write(c_socket, greeting, strlen(greeting));
-			pthread_create(&thread, NULL, do_chat, (void *) &c_socket);
+			th_id = pthread_create(&th, NULL, doChat, (void*)&c_socket);
 		}
 	}
 }
 
-void *do_chat(void *arg)
-{
+void *doChat(void *arg){
 	int c_socket = *((int *)arg);
+	char nickname[20];
+	char room[20] = "all";
 	char chatData[CHATDATA];
+	char writeData[CHATDATA];
 	int i, n;
-	while(1) {
+	char *ptr;
+	char target[20];
+	for(i=0; i<MAX_CLIENT; i++){
+		if(listClient[i].c_socket==c_socket){
+			strcpy(nickname, listClient[i].nickname);
+		}
+	}
+	while(1){
 		memset(chatData, 0, sizeof(chatData));
-		if((n = read(c_socket, chatData, sizeof(chatData))) > 0) {
-			char *token = NULL;
-			char *nickname = NULL;
-			char *message = NULL;
-			if(strncasecmp(chatData, WHISPER, 2) == 0){	
-				token = strtok(chatData, DELIMETER); // \w
-				nickname = strtok(NULL, DELIMETER); //nickname
-				message = strtok(NULL, "\0"); 
-			}
-			for(i = 0; i < MAX_CLIENT; i++) {
-				if(clientList[i].c_socket != INVALID_SOCK) {
-					if(nickname != NULL){
-						if(strcasecmp(clientList[i].nickname, nickname) == 0)
-							write(clientList[i].c_socket, message, strlen(message));
-					}else{
-						write(clientList[i].c_socket, chatData, n);
-					}
-				}
-			}
-			if(strstr(chatData, escape) != NULL) {
+		if((n = read(c_socket, chatData, sizeof(chatData)))>0){
+			sprintf(writeData, "[대화방:%s] %s : %s", room, nickname, chatData);
+			if(strstr(chatData, "/exit") != NULL){
 				popClient(c_socket);
 				break;
 			}
+			else if(strncasecmp(chatData, "/help", 5)==0){
+				sprintf(writeData, "===도움말===\n  /list  ----- 접속자 보기\n  /w [닉네임] [내용] ----- 귓속말 하기\n  /join [방이름] ----- 대화방 들어가기(기본 대화방 : all)\n  /exit ----- 접속 종료하기\n");
+				write(c_socket, writeData, strlen(writeData));
+			}
+			else if(strncasecmp(chatData, "/list", 5)==0){
+				sprintf(writeData, "===접속자 명단===\n");
+				write(c_socket, writeData, strlen(writeData));
+				for(i=0; i<MAX_CLIENT; i++){
+					if(listClient[i].c_socket!=INVALID_SOCK){
+						sprintf(writeData, "[%s] 참여중인 대화방 : %s\n", listClient[i].nickname, listClient[i].room);
+						write(c_socket, writeData, strlen(writeData));
+					}
+				}
+			}
+			else if(strncasecmp(chatData, "/w", 2)==0){
+				strtok(chatData, " ");
+				strcpy(target, strtok(NULL, " "));
+				strcpy(chatData, strtok(NULL, "\0"));
+				sprintf(writeData, "[%s님의 귓속말] %s", nickname, chatData);
+				for(i=0; i<MAX_CLIENT; i++){
+					if(strcasecmp(target, listClient[i].nickname)==0){
+						write(listClient[i].c_socket, writeData, strlen(writeData));
+					}
+				}
+			}
+			else if(strncasecmp(chatData, "/join", 5)==0){
+				strtok(chatData, " ");
+				strcpy(room, strtok(NULL, "\n"));
+				sprintf(writeData, "[%s 님이 %s방에 입장하였습니다. (전체방으로 돌아가기 : /join all)]\n", nickname, room);
+				for(i=0; i<MAX_CLIENT; i++){
+					if(listClient[i].c_socket==c_socket){
+						strcpy(listClient[i].room, room);
+					}
+				}
+				for(i=0; i<MAX_CLIENT; i++){
+					if(strcasecmp(listClient[i].room, room)==0){
+						write(listClient[i].c_socket, writeData, strlen(writeData));
+					}
+				}
+			}
+			else{
+				for(i=0; i<MAX_CLIENT; i++){
+					if(listClient[i].c_socket!=INVALID_SOCK){
+						if(strcasecmp(listClient[i].room, room)==0){
+							write(listClient[i].c_socket, writeData, strlen(writeData));
+						}
+					}
+				}
+			}
 		}
 	}
 }
 
-int pushClient(int c_socket, char *nickname) {
-	int i;
-	for(i = 0; i < MAX_CLIENT; i++) {
-		if(clientList[i].c_socket == INVALID_SOCK) {
-			pthread_mutex_lock(&mutex);
-			clientList[i].c_socket = c_socket;
-			memset(clientList[i].nickname, 0, CHATDATA);
-			strcpy(clientList[i].nickname, nickname);
-			pthread_mutex_unlock(&mutex);
-			return i;
-		}
-	}
-	if(i == MAX_CLIENT)
-		return -1;
-}		
+int pushClient(int c_socket){
+	int i, j;
+	char buf[20];
+	char writeData[CHATDATA];
 
-int popClient(int c_socket)
-{			
-	int i;	 
-	close(c_socket); 
-	for(i = 0; i < MAX_CLIENT; i++) {
-		if(c_socket == clientList[i].c_socket) {
-			pthread_mutex_lock(&mutex);
-			clientList[i].c_socket = INVALID_SOCK;
-			pthread_mutex_unlock(&mutex);
-			break;
+	memset(buf, 0, sizeof(buf));
+	read(c_socket, buf, sizeof(buf));
+	for(i=0; i<MAX_CLIENT; i++){
+		if(listClient[i].c_socket==INVALID_SOCK){
+			listClient[i].c_socket = c_socket;
+			memset(listClient[i].nickname, 0, sizeof(listClient[i].nickname));
+			strcpy(listClient[i].nickname, buf);
+			memset(listClient[i].room, 0, sizeof(listClient[i].room));
+			strcpy(listClient[i].room, "all");
+
+			sprintf(writeData, "[%s]님께서 채팅방 프로그램에 접속하셨습니다.! (도움말 /help)\n", buf);
+			for(j=0; j<MAX_CLIENT; j++){
+				if(listClient[j].c_socket!=INVALID_SOCK){
+				write(listClient[j].c_socket, writeData, strlen(writeData));
+				}
+			}
+			return 0;
 		}
 	}
-	return 0;
+
+	return -1;
+}
+
+int popClient(int c_socket){
+	int i, j;
+	char writeData[CHATDATA];
+	for(i=0; i<MAX_CLIENT; i++){
+		if(listClient[i].c_socket==c_socket){
+			sprintf(writeData, "[%s]님께서 채팅 프로그램 접속을 종료하셨습니다!\n", listClient[i].nickname);
+			for(j=0; j<MAX_CLIENT; j++){
+				if(listClient[j].c_socket!=INVALID_SOCK){
+					write(listClient[j].c_socket, writeData, strlen(writeData));
+				}
+			}
+			listClient[i].c_socket = INVALID_SOCK;
+			strcpy(listClient[i].nickname, "\0");
+			strcpy(listClient[i].room, "\0");
+
+		}
+	}
+	close(c_socket);
 }
